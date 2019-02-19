8b5fec0cf5841784f51adbbe4ed20c885a921bbd
Burhanuddin Baharuddin <burhanclj@gmail.com>
2019-02-19 22:22:19 +0800

    Move top ads below title

--- injectAds.js
+++ injectAds.js
@@ -5,12 +5,12 @@ var topAds = '<ins class="adsbygoogle" ' +
     'data-ad-format="auto" ' +
     'data-full-width-responsive="true"></ins>';
 
-var middleAds = '<ins class="adsbygoogle" ' +
-    'style="display:block; text-align:center;" ' +
-    'data-ad-layout="in-article" ' +
-    'data-ad-format="fluid" ' +
-    'data-ad-client="ca-pub-8041854815282465" ' +
-    'data-ad-slot="6888829839"></ins>';
+// var middleAds = '<ins class="adsbygoogle" ' +
+//     'style="display:block; text-align:center;" ' +
+//     'data-ad-layout="in-article" ' +
+//     'data-ad-format="fluid" ' +
+//     'data-ad-client="ca-pub-8041854815282465" ' +
+//     'data-ad-slot="6888829839"></ins>';
 
 var bottomAds = '<ins class="adsbygoogle" ' +
     'style="display:block" ' +
@@ -20,7 +20,12 @@ var bottomAds = '<ins class="adsbygoogle" ' +
     'data-full-width-responsive="true"></ins>';
 
 function injectAds(page) {
-    page.content = topAds + page.content + bottomAds;
+    page.content =
+        page.content
+        .replace(page.title + '</h1>',
+                 page.title + '</h1>' + topAds) +
+        bottomAds;
+
     return page;
 }
 
