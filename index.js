5bd308f0a8c72c98ba74cc36eb485ba281d57d86
Burhanuddin Baharuddin <burhanclj@gmail.com>
2019-02-19 22:39:35 +0800

    Remove book config

--- index.js
+++ index.js
@@ -3,11 +3,5 @@ var injectAds = require('./injectAds');
 module.exports = {
     hooks: {
         "page": injectAds
-    },
-    book: {
-        assets: './assets',
-        js: [
-            'adsense.js'
-        ]
     }
 };
